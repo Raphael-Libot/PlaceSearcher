7b63e3b31574cf777d019472992cfa75e296d637
Raphael Libot <raphael.libot@gmail.com>
2018-01-08 16:11:48 +0100

    liste V1

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -3,6 +3,8 @@ package org.miage.placesearcher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -12,34 +14,23 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.ratingBar)
-    RatingBar ratingBar;
+    @BindView(R.id.items)
+    ListView itemsView;
 
-    @BindView(R.id.textView)
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        createList();
     }
 
-    @OnClick(R.id.textView)
-    public void decreaseRating(){
-        float actualRating = this.ratingBar.getRating();
-        if (actualRating > 0) {
-            this.ratingBar.setRating(actualRating - 1);
-        }
+    private void createList(){
+        String[] listItems = new String[]{"Veni","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici","Vidi","Vici"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        itemsView.setAdapter(adapter);
     }
 
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-        float actualRating = this.ratingBar.getRating();
-        if (actualRating < this.ratingBar.getMax()) {
-            this.ratingBar.setRating(actualRating + 1);
-        }
-    }
 }
