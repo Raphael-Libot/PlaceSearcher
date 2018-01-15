05f20a88b3979eef2360aadbbe3660e68f63e619
Simon <simon44360@gmail.com>
2018-01-15 16:25:24 +0100

    AsyncTask for http request

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,8 +22,14 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String GET_URL = "https://api-adresse.data.gouv.fr/search/?q=Place%20du%20commerce";
+    private static final String TAG = MainActivity.class.toString();
 
     @BindView(R.id.items)
     ListView itemsView;
@@ -72,4 +80,35 @@ public class MainActivity extends AppCompatActivity {
         return this;
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AsyncTask<String, Void, Response> asyncTask = new AsyncTask<String, Void, Response>() {
+            @Override
+            protected Response doInBackground(String... strings) {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(strings[0])
+                        .build();
+                Response response = null;
+                try {
+                    response = okHttpClient.newCall(request).execute();
+                } catch (IOException e) {
+                    // Silent catch
+                }
+                return response;
+            }
+
+            @Override
+            protected void onPostExecute(Response response) {
+                super.onPostExecute(response);
+                try {
+                    Log.i(TAG, response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        asyncTask.execute(GET_URL);
+    }
 }
